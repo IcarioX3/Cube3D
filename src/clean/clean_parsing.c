e173850d33097822571a505b3a1dd53ccfeb800f
Franck GOJARD <frgojard@paul-f4Ar1s3.clusters.42paris.fr>
2023-08-25 15:51:20 +0200

    finish mandatory

--- src/clean/clean_parsing.c
+++ src/clean/clean_parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   clean_parsing.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: icario <icario@student.42.fr>              +#+  +:+       +#+        */
+/*   By: frgojard <frgojard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 10:30:05 by icario            #+#    #+#             */
-/*   Updated: 2023/08/24 10:54:58 by icario           ###   ########.fr       */
+/*   Updated: 2023/08/25 15:23:42 by frgojard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	clean_parsing(t_game *game)
 {
-	int	a;
-	
-	a = 0;
-	while (a < 4)
+	int i;
+
+	i = 0;
+	while (i < 4)
 	{
-		free(game->vars->file_texture[a]);
-		a++;
+		free(game->vars->file_texture[i]);
+		i++;
 	}
 	free(game->vars->file_texture);
 	free(game->vars->mapline);
@@ -31,4 +31,30 @@ void	clean_parsing(t_game *game)
 	if (game->vars->ceiling_info)
 		free(game->vars->ceiling_info);
 	free(game->vars);
+}
+void	clean_parsing1(t_vars **vars)
+{
+	int i;
+
+	i = 0;
+	if ((*vars)->file_texture)
+	{
+		while (i < 4)
+		{
+			free((*vars)->file_texture[i]);
+			i++;
+		}
+	}
+	free((*vars)->file_texture);
+	if ((*vars)->mapline)
+		free((*vars)->mapline);
+	if ((*vars)->map)
+		free_double_tab((*vars)->map);
+	if ((*vars)->map_info)
+		free_double_tab((*vars)->map_info);
+	if ((*vars)->floor_info)
+	 	free((*vars)->floor_info);
+	if ((*vars)->ceiling_info)
+	 	free((*vars)->ceiling_info);
+	free((*vars));
 }
--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: franck <franck@student.42.fr>              +#+  +:+       +#+        */
+/*   By: frgojard <frgojard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 08:52:56 by frgojard          #+#    #+#             */
-/*   Updated: 2023/08/23 20:30:45 by franck           ###   ########.fr       */
+/*   Updated: 2023/08/25 15:45:22 by frgojard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,33 @@ int	parsing(t_vars **vars)
 		return (1);
 	if (check_texture(vars) == 1)
 		return (1);
+	return (0);
+}
+
+int	init_parsing(t_vars **vars, char **av, int fd)
+{	
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+		return (write(2, "error: can't open the file\n", 28), 1);
+	*vars = malloc(sizeof(t_vars));
+	if (!vars)
+		return (1);
+	(*vars)->mapline = get_map(fd);
+	if ((*vars)->mapline == NULL)
+	{
+		close(fd);
+		return (free(*vars), 1);
+	}
+	(*vars)->map = ft_split((*vars)->mapline, '\n');
+	if ((*vars)->map == NULL)
+	{
+		free((*vars)->mapline);
+		return (free(*vars), 1);
+	}
+	(*vars)->ceiling_info = NULL;
+	(*vars)->floor_info = NULL;
+	(*vars)->file_texture = NULL;
+	(*vars)->file_texture = malloc(4 * sizeof(char *));
+	close(fd);
 	return (0);
 }
